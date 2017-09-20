762428b2251e17c989203e8388b16dbeff0cbb84
Mark Tawara <mhtawara@gmail.com>
2017-09-19 20:26:25 -0400

    Add files via upload

---
+++ WebContent/scripts/template.js
@@ -0,0 +1,5 @@
+$(function(){
+	$("#header").load("templates/template.html #header");
+	$("#menu").load("templates/template.html #menu");
+	$("#footer").load("templates/template.html #footer");
+});
